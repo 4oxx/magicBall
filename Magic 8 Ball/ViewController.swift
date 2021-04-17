180cb8c4048ecea23ae8b627dbc1149e48b617aa
Oleksii Bomersbah <82603311+4oxx@users.noreply.github.com>
2021-04-17 12:56:28 +0300

    Add files via upload

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,16 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let ballArray = [#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball3")]
+    
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        
+        imageView.image = ballArray.randomElement()
+    }
+}
+
